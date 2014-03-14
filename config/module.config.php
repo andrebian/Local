d765ca35f5f514d2b322236adc46ad11608733bf
Andre Cardoso <andrecardosodev@gmail.com>
2014-03-14 13:18:53 -0300

    Adicionado Partial para consulta por CEP

--- config/module.config.php
+++ config/module.config.php
@@ -2,4 +2,11 @@
 
 namespace Local;
 
-return array();
+return array(
+    'view_manager' => array(
+        'doctype' => 'HTML5',
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+);
